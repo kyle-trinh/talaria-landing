3ca8264141eb3d00090ea79615d72788ad4a0114
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-07-17 11:17:50 -0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,87 @@
+const config = require("./src/config")
+
+module.exports = {
+  siteMetadata: {
+    title: config.siteTitle,
+    description: config.siteDescription,
+    siteUrl: config.siteUrl,
+    author: `@binhthaitrinh`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Talaria Order",
+        short_name: "TalariaOrder",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: config.colors.primary,
+        display: "standalone",
+        icon: "src/images/logo.png",
+      },
+    },
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            // https://www.gatsbyjs.org/packages/gatsby-remark-external-links
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "nofollow noopener noreferrer",
+            },
+          },
+          {
+            // https://www.gatsbyjs.org/packages/gatsby-remark-images
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 700,
+              linkImagesToOriginal: true,
+              quality: 90,
+              tracedSVG: { color: config.colors.green },
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-mdx`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteTitle: "Talaria Order",
+  siteUrl: "https://www.talaria.com",
+  siteDescription:
+    "Talaria là công ty dropshipping với mục tiêu đem lại sự tự do mua sắm cho người Việt. Chúng tôi luôn cam kết: đảm bảo nguồn cung chính hãng với mức giá ưu đãi nhất",
+  siteKeywords: "Talaria, Talaria Order, order đồ Mỹ, order Amazon",
+  siteLanguage: "vn_VN",
+  email: "hello@talariaorder.com",
+  phoneNumber: "+84 33 894 6668",
+  socialMedia: [
+    {
+      name: "Facebook",
+      url: "https://www.facebook.com/talariaorder/",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/talariaorder/",
+    },
+  ],
+  navLinks: [
+    {
+      name: "Giới thiệu",
+      url: "/#gioi-thieu",
+    },
+    {
+      name: "Hướng dẫn order",
+      url: "/#huong-dan-order",
+    },
+    {
+      name: "Phản hồi của khách",
+      url: "/#phan-hoi",
+    },
+  ],
+
+  colors: {
+    primary: "#4AA99F",
+    primaryDark: "#25746B",
+    secondary: "#F9F7BD",
+  },
+}
