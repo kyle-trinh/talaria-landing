3c730f8792c5e1fc664353a387052d9928279d30
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-07-19 17:11:26 -0700

    add footer

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,54 @@
+import React from "react"
+import { FacebookIcon, InstagramIcon, MailIcon, PhoneIcon } from "@icons"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { socialMedia, email, phoneNumber, siteUrl } from "@config"
+
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fixed(width: 80, height: 80) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer__social-links">
+          {socialMedia.map((social, i) => (
+            <a key={i} href={social.url} className="footer__social-link">
+              {social.name === "Facebook" ? (
+                <FacebookIcon />
+              ) : (
+                <InstagramIcon />
+              )}
+            </a>
+          ))}
+          <a href={`mailto: ${email}`} className="footer__social-link">
+            <MailIcon />
+          </a>
+        </div>
+        <div className="footer__logo-box">
+          <a href={siteUrl}>
+            <Img
+              fixed={data.file.childImageSharp.fixed}
+              className="footer__logo"
+              alt="Talaria logo"
+            />
+          </a>
+        </div>
+        <div className="footer__phone">
+          <PhoneIcon />
+          <p>{phoneNumber}</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header"
+import Footer from "./Footer"
 import "../../styles/main.scss"
 
 const Layout = ({ children }) => {
@@ -28,11 +29,7 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
 
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
+      <Footer />
     </>
   )
 }
