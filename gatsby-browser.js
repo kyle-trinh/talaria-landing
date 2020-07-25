72547b4607560b047b0872bcce10f1c8298e723b
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2020-07-25 00:48:43 -0700

    fix responsive for swiper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,10 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
-exports.onRouteUpdate = function ({ location }) {
-  // Don't track while developing.
-  if (process.env.NODE_ENV === `production` && typeof fbq === `function`) {
-    fbq("track", "ViewContent")
-  }
-}
+// // You can delete this file if you're not using it
+// exports.onRouteUpdate = function ({ location }) {
+//   // Don't track while developing.
+//   if (process.env.NODE_ENV === `production` && typeof fbq === `function`) {
+//     fbq("track", "ViewContent")
+//   }
+// }
